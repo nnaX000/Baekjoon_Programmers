cd40b0e5b19a7006c23965a17e36eccbaf47187b
김나영 <kny200208@naver.com>
2025-06-25 17:00:43 +0900

    [level 3] Title: 단어 변환, Time: 3.35 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,26 +1,37 @@
 from collections import deque
+
 def solution(begin, target, words):
-    count = 0
-    dequee=deque()
-    dequee.append([begin,list([begin])])
-    def DFS(dequee,target,words):
-        count = 0
-        while(dequee):
-            size=len(dequee)
-            count+=1
-            for i in range(size):
-                tmp=dequee.popleft()
-                for idx,j in enumerate(words):
-                    num = 0
-                    for k in range(len(j)):
-                        if(j[k]==tmp[0][k]):
-                            num+=1
-                    if(num==len(j)-1 and (j not in tmp[1])):
-                        if(j==target):
-                            return count
-                        else:
-                            k=list(tmp[1])
-                            k.append(j)
-                            dequee.append([j,k])
+    answer=0
+    set_words=set(words)
+    dequee=deque(begin)
+    dequee=deque([[begin,answer]])
+    stop=False
+    
+    if(target not in set_words):
         return 0
-    return DFS(dequee,target,words)
+    
+    while(dequee):
+        word,answer=dequee.popleft()
+        
+        for i in words:
+            count=0
+            for jdx,j in enumerate(i):
+                if(j!=word[jdx]):
+                    count+=1
+
+            if(count==1):
+                if(i==target):
+                    stop=True
+                    break
+                else:
+                    dequee.append([i,answer+1])
+        
+        if(stop):
+            answer+=1
+            break
+            
+    return answer
+            
+    
+
+                
