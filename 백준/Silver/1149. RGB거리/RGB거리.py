db12f8f88560e607ffa7df8cd7a4095443a28bae
김나영 <kny200208@naver.com>
2025-06-29 18:37:22 +0900

    [Silver I] Title: RGB거리, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,13 +1,21 @@
-#[빨,초,파]
-num=int(input())
+N=int(input())
 array=[]
-for i in range(num):
-    temp=list(map(int,input().split(' ')))
-    array.append(temp)
-dp=[[0,0,0] for i in range(num)]
-dp[0]=array[0]
-for i in range(1,num):
-    dp[i][0]=min(dp[i-1][1]+array[i][0],array[i][0]+dp[i-1][2])
-    dp[i][1]=min(dp[i-1][0]+array[i][1],array[i][1]+dp[i-1][2])
-    dp[i][2] = min(dp[i - 1][0] + array[i][2], array[i][2] + dp[i - 1][1])
-print(min(dp[-1]))
+
+for i in range(N):
+    array.append(list(map(int,input().split(' '))))
+
+table=[[0 for i in range(3)] for j in range(N)]
+
+for i in range(3):
+    table[0][i]=array[0][i]
+
+for i in range(1,N):
+    for j in range(3):
+        if(j==0):
+            table[i][j]=min(array[i][j]+table[i-1][1],array[i][j]+table[i-1][2])
+        elif(j==1):
+            table[i][j]=min(array[i][j]+table[i-1][0],array[i][j]+table[i-1][2])
+        else:
+            table[i][j]=min(array[i][j]+table[i-1][0],array[i][j]+table[i-1][1])
+
+print(min(table[N-1]))
