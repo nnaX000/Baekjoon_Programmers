23ea046ee9d9aeb2a5474c6bd828f8a414c2d94b
김나영 <kny200208@naver.com>
2025-07-23 01:46:45 +0900

    [Silver III] Title: 분수 합, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.py
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 a_1,b_1=map(int,sys.stdin.readline().rstrip().split(' '))
 a_2,b_2=map(int,sys.stdin.readline().rstrip().split(' '))
@@ -6,11 +7,12 @@ a_2,b_2=map(int,sys.stdin.readline().rstrip().split(' '))
 top=(a_1*b_2)+(b_1*a_2)
 bottom=(b_1*b_2)
 
-for i in range(2,int(min(top,bottom)**1/2)+1):
-    if(top%i==0 and bottom%i==0):
-        while(top%i==0 and bottom%i==0):
-            top//=i
-            bottom//=i
+array=[]
+
+gcd=math.gcd(top,bottom)
+
+top//=gcd
+bottom//=gcd
 
 print(top,end=" ")
 print(bottom)
