85ec6c4eb58f1c188e24f724c6ecaa3737eacffa
김나영 <kny200208@naver.com>
2025-08-22 00:33:17 +0900

    [Gold IV] Title: 이모티콘, Time: 132 ms, Memory: 64776 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -8,25 +8,29 @@ S=int(sys.stdin.readline().rstrip())
 #화면에 있는 이모티콘 중 하나 삭제
 
 dequee=deque()
-dequee.append([1,0,0])#[화면에 있는 임티 개수, 클립보드 임티 개수, 시간]
+dequee.append([1,0])#[화면에 있는 임티 개수, 클립보드 임티 개수]
 dist = [[-1]*(2000) for _ in range(2000)]
+dist[1][0]=0
 
 while(dequee):
-    tmp=dequee.popleft()
+    s,c=dequee.popleft()
+    t=dist[s][c]
 
-    if(tmp[0]==S):
-        print(tmp[2])
+    if(s==S):
+        print(dist[s][c])
         break
     
-    if(tmp[0]>0):
-        if(0<=tmp[0]-1<2000 and dist[tmp[0]][tmp[0]]==-1):
-            dequee.append([tmp[0],tmp[0],tmp[2]+1])
-            dist[tmp[0]][tmp[0]]=tmp[2]+1
-        if(dist[tmp[0]-1][tmp[1]]==-1):
-            dequee.append([tmp[0]-1,tmp[1],tmp[2]+1])
-            dist[tmp[0]-1][tmp[1]]=tmp[2]+1
+    #화면에 있는 임티 복사
+    if(dist[s][s]==-1):
+        dequee.append([s,s])
+        dist[s][s]=t+1
 
-    if(tmp[1]>0):
-        if(0<=tmp[0]+tmp[1]<2000 and dist[tmp[0]+tmp[1]][tmp[1]]==-1):
-            dequee.append([tmp[0]+tmp[1],tmp[1],tmp[2]+1])
-            dist[tmp[0]+tmp[1]][tmp[1]]=tmp[2]+1
+    #클립보드에 있는거 붙여넣기
+    if(c>0 and 0<=s+c<2000 and dist[s+c][c]==-1):
+        dequee.append([s+c,c])
+        dist[s+c][c]=t+1
+
+    #화면에 있는 이모티콘 중 하나 삭제
+    if((s>0) and 0<=s-1<2000 and dist[s-1][c]==-1):
+        dequee.append([s-1,c])
+        dist[s-1][c]=t+1
