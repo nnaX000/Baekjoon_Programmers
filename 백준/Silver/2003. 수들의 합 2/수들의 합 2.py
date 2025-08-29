9ee2271bfce8a367a5ebd8145b92cffaa17593d1
김나영 <kny200208@naver.com>
2025-08-29 23:19:11 +0900

    [Silver IV] Title: 수들의 합 2, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -4,16 +4,34 @@ N,M=map(int,sys.stdin.readline().rstrip().split(' '))
 
 A=list(map(int,sys.stdin.readline().rstrip().split(' ')))
 
-sum_value=[0 for i in range(N+1)]
-tmp=0
+# answer=0
+
+# for i in range(N):
+#     sum_value=0
+#     for j in range(i,N):
+#         sum_value+=A[j]
+#         if(sum_value==M):
+#             answer+=1
+#             break
+#         if(sum_value>M):
+#             break
+
+# print(answer)
+
+start,end=0,0
 answer=0
+sum_value=0
 
-for i in range(N):
-    tmp+=A[i]
-    sum_value[i+1]=tmp
-
-for i in range(N+1):
-    for j in range(0,i):
-        if(sum_value[i]-sum_value[j]==M):
+while True:
+    if(sum_value>=M):
+        if(sum_value==M):
             answer+=1
+        sum_value-=A[start]
+        start+=1
+    elif(end==N):
+        break
+    else:
+        sum_value+=A[end]
+        end+=1
+
 print(answer)
