efb6a7386553ecff855c57403980a9acad3c81d4
김나영 <kny200208@naver.com>
2025-09-02 15:35:10 +0900

    [Gold III] Title: 육각 보드, Time: 36 ms, Memory: 32824 KB -BaekjoonHub

--- 백준/Gold/12946. 육각 보드/육각 보드.py
+++ 백준/Gold/12946. 육각 보드/육각 보드.py
@@ -6,35 +6,35 @@ N=int(sys.stdin.readline().rstrip())
 
 board=[list(sys.stdin.readline().rstrip()) for i in range(N)]
 
-visited=[[0 for i in range(N)] for j in range(N)]
+visited=[[-1 for i in range(N)] for j in range(N)]
 
 dx=[0,0,1,1,-1,-1]
 dy=[-1,1,-1,0,0,1]
 
 answer=0
 
-def dfs(x,y):
+def dfs(x,y,color):
     global answer
     global visited
-    answer=max(answer,1)
-    
+    answer=max(answer,1) #어쨌든 X가 하나있으면 무조건 1개 색은 칠해야 함.
+
     for i in range(6):
         if(0<=x+dx[i]<N and 0<=y+dy[i]<N and board[x+dx[i]][y+dy[i]]=="X"):
             nx=x+dx[i]
             ny=y+dy[i]
-            if(visited[nx][ny]==0):
-                visited[nx][ny]=-visited[x][y]
-                dfs(nx,ny)
-                answer=max(answer,2)
+            if(visited[nx][ny]==-1):
+                visited[nx][ny]=color
+                dfs(nx,ny,1-color)
+                answer=max(answer,2) # 인접한게 있으면 무조건 2개 색 필요함.
             else:
-                if visited[nx][ny]==visited[x][y]:
-                    answer=max(answer,3)
+                if visited[nx][ny]==1-color:
+                    answer=max(answer,3) #이미 칠해져있는데 그게 내 색이랑 똑같으면 3개 색 필요함.
                     return
     
 for i in range(N):
     for j in range(N):
-        if(board[i][j]=="X" and visited[i][j]==0):
-            visited[i][j]=1
-            dfs(i,j)
+        if(board[i][j]=="X" and visited[i][j]==-1):
+            visited[i][j]=0
+            dfs(i,j,1)
 
 print(answer)
