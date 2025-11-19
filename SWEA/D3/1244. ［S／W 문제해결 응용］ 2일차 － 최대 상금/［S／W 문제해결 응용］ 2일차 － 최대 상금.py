f3409885bebcdb5cd6d5d2289a7ad79dffa7201f
김나영 <kny200208@naver.com>
2025-11-19 17:10:50 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 103 ms, Memory: 61,056 KB -BaekjoonHub

---
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -0,0 +1,75 @@
+# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+# 아래 표준 입출력 예제 필요시 참고하세요.
+
+# 표준 입력 예제
+'''
+a = int(input())                        정수형 변수 1개 입력 받는 예제
+b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제 
+d = float(input())                      실수형 변수 1개 입력 받는 예제
+e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
+h = input()                             문자열 변수 1개 입력 받는 예제
+'''
+
+# 표준 출력 예제
+'''
+a, b = 6, 3
+c, d, e = 1.0, 2.5, 3.4
+f = "ABC"
+print(a)                                정수형 변수 1개 출력하는 예제
+print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
+print(c, d, e)                          실수형 변수 3개 출력하는 예제
+print(f)                                문자열 1개 출력하는 예제
+'''
+
+
+
+
+'''
+아래의 구문은 input.txt 를 read only 형식으로 연 후,
+앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
+아래 구문을 사용하기 위해서는 import sys가 필요합니다.
+단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
+'''
+#import sys
+#sys.stdin = open("input.txt", "r")
+
+T=int(input())
+
+for i in range(T):
+    origin, count = input().split(' ')
+    origin = list(origin)
+    count = int(count)
+    visited=set()
+    visited.add((int(''.join(origin)),0))
+
+    max_result=float('-inf')
+
+    def dfs(num,cost):
+        global max_result
+
+        #print("cost",cost,"num",num)
+
+        if(cost==count):
+            max_result=max(max_result,int(''.join(num)))
+            return
+
+        for i in range(len(num)-1):
+            for j in range(i+1,len(num)):
+                n_num=num[:]
+
+                tmp=n_num[i]
+                n_num[i]=n_num[j]
+                n_num[j]=tmp
+
+                candi = int(''.join(n_num))
+
+                if((candi,cost+1) not in visited):
+                    visited.add((candi,cost+1))
+                    dfs(n_num,cost+1)
+
+    dfs(origin,0)
+
+    print(f"#{i+1} {max_result}")
