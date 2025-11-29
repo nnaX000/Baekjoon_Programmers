753e3f28bea610fdb3e047a86445f146d01641be
김나영 <kny200208@naver.com>
2025-11-29 22:58:16 +0900

    [Silver II] Title: 과자 나눠주기, Time: 3924 ms, Memory: 142508 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,24 @@
+import sys
+
+M,N=map(int,sys.stdin.readline().rstrip().split(' '))
+L=list(map(int,sys.stdin.readline().rstrip().split(' ')))
+
+start=0
+end=max(L)+1
+answer=0
+
+while(start<=end):
+    middle=(start+end)//2
+    tmp=0
+
+    if(middle>0):
+        for i in L:
+            tmp+=(i//middle)
+
+    if(tmp>=M):
+        answer=max(middle,answer)
+        start=middle+1
+    else:
+        end=middle-1
+
+print(answer)
