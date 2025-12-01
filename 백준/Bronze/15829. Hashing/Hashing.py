08da98b32fe7c454559bfe756f92d4493fa244f4
김나영 <kny200208@naver.com>
2025-12-01 17:15:35 +0900

    [Bronze II] Title: Hashing, Time: 64 ms, Memory: 34908 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,22 @@
+import sys
+from collections import defaultdict
+
+alpha=defaultdict(int)
+
+#r은 31, M은 1234567891
+
+L_len=int(sys.stdin.readline().rstrip())
+L=sys.stdin.readline().rstrip()
+
+num=1
+answer=0
+
+for i in range(97,123):
+    alpha[chr(i)]=num
+    num+=1
+
+for idx,i in enumerate(L):
+    num=alpha[i]
+    answer+=((num)*(31**idx))%1234567891
+
+print(answer)
