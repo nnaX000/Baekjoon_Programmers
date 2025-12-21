66201ed335e523078b9dbbab5c862502fab8b6fa
김나영 <kny200208@naver.com>
2025-12-21 17:53:33 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,17 +1,30 @@
-#[첫번째인경우,두번째인경우,max] [50,0,0] [20,70,50]
-# [95,65,70] [150,
-num=int(input())
-array=[0,0]
-for i in range(num):
-    temp=int(input())
-    array.append(temp)
-array.append(0)
-array.append(0)
-dp=[[0,0,0] for i in range(num+4)]
-dp[2][0]=array[2]
-dp[2][2]=array[2]
-for i in range(3,num+2):
-    dp[i][0]=dp[i-2][2]+array[i]
-    dp[i][1]=dp[i-1][0]+array[i]
-    dp[i][2]=max(dp[i][0],dp[i][1])
-print(max(dp[num+1]))
+import sys
+
+input=sys.stdin.readline
+
+N=int(input().rstrip())
+
+stair=[]
+
+for i in range(N):
+    stair.append(int(input().rstrip()))
+
+stair.reverse()
+dp=[[0,0] for i in range(len(stair))]
+dp[0][0]=stair[0]
+max_value=float('-inf')
+
+for i in range(1,len(dp)):
+    if(i==1):
+        dp[i][1]=dp[i-1][0]+stair[i]
+    elif(i==2):
+        dp[i][0]=max(dp[i-2])+stair[i]
+    else:
+        dp[i][0]=max(dp[i-2])+stair[i]
+        dp[i][1]=dp[i-1][0]+stair[i]
+
+for i in range(len(dp)):
+    for j in range(2):
+        max_value=max(max_value,dp[i][j])
+
+print(max_value)
