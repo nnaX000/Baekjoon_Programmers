70a967a9db0b6e66294b22205739f40d20742036
김나영 <kny200208@naver.com>
2025-12-22 00:05:23 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,33 +1,27 @@
 import sys
 
 T=int(sys.stdin.readline().rstrip())
+count=0
 
-def dfs(num,sum_value):
-    global method
-    if(sum_value==num):
-        method+=1
-        return
+def dfs(n):
+    global count
+
+    if(n==0):
+        count+=1
+
+    if(n-1>=0):
+        dfs(n-1)
     
-    if(sum_value+1<=num):
-        sum_value+=1
-        dfs(num,sum_value)
-        sum_value-=1
+    if(n-2>=0):
+        dfs(n-2)
 
-    if(sum_value+2<=num):
-        sum_value+=2
-        dfs(num,sum_value)
-        sum_value-=2
-
-    if(sum_value+3<=num):
-        sum_value+=3
-        dfs(num,sum_value)
-        sum_value-=3
-
-    return method
+    if(n-3>=0):
+        dfs(n-3)
 
 for i in range(T):
     tmp=int(sys.stdin.readline().rstrip())
-    method=0
-    sum_value=0
-    result=dfs(tmp,sum_value)
-    print(result)
+    count=0
+
+    dfs(tmp)
+
+    print(count)
