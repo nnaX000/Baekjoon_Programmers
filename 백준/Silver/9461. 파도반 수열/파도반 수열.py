eb881bc7bb9df2bdbb5180cd4499f2ce43f8a418
김나영 <kny200208@naver.com>
2025-12-25 18:54:28 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,16 +1,23 @@
-#1 1 1 2 2 3 4 5 7 9 12 16(+4) 21(+5) 28(+7) 37(+9) 49(+12)
-#1 2 3 4 5 6 7 8 9 10
-answer=[]
-basic=[0]*1000
-basic[0:5]=[1,1,1,2,2]
-num=int(input())
-for i in range(num):
-    a=int(input())
-    if(basic[a]!=0):
-        answer.append(basic[a-1])
-    else:
-        for j in range(5,a):
-            basic[j]=basic[j-1]+basic[j-5]
-        answer.append(basic[a-1])
-for i in answer:
-    print(i)
+import sys
+
+T=int(sys.stdin.readline().rstrip())
+array=[1,1,1,2,2,3,4,5,7,9]
+
+tmp=[]
+
+for i in range(T):
+    N=int(sys.stdin.readline().rstrip())
+    tmp.append(N)
+
+max_value=max(tmp)
+
+left=5
+right=9
+
+for i in range(max_value-9):
+    array.append(array[left]+array[right])
+    left+=1
+    right+=1
+
+for i in tmp:
+    print(array[i-1])
