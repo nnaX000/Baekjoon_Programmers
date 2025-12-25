3db5b54c4dc2e5bc5db128895225535ea5c7ee17
김나영 <kny200208@naver.com>
2025-12-25 20:18:42 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 32776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,40 +1,38 @@
-#    01
-#10  11  12
-#    21
-#node=[방문했으면 True,안했으면 False]
 import sys
-from collections import deque
-dx=[1,-1,0,0]
-dy=[0,0,1,-1]
-testCaseNum=int(sys.stdin.readline())
-answer=[]
-for i in range(testCaseNum):
-    count=0
-    dequee = deque()
-    garo,sero,where=map(int,sys.stdin.readline().split(' '))
-    array = [[0 for i in range(sero+1)] for j in range(garo+1)]
-    for j in range(where):
-        tmp=list(map(int,sys.stdin.readline().split(' ')))
-        array[tmp[0]][tmp[1]]=1
-    for k in range(garo):
-        for i in range(sero):
-            if(array[k][i]==1):
-                count+=1
-                dequee.append([k,i])
-                array[k][i]=0
-                while(dequee):
-                    start = dequee.popleft()
-                    for i in range(4):
-                        targetX = start[0] + dx[i]
-                        targetY = start[1] + dy[i]
-                        if (targetX < 0 or targetY < 0 or targetX >= garo or targetY >= sero):
-                            continue
-                        if (array[targetX][targetY] == 1):
-                            dequee.append([targetX, targetY])
-                            array[targetX][targetY] = 0
 
-    answer.append(count)
+sys.setrecursionlimit(10**6)
 
-for i in answer:
-    print(i)
+def dfs(x,y):
+    global cabbage
 
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+
+        if(0<=nx<M and 0<=ny<N and cabbage[nx][ny]==1):
+            cabbage[nx][ny]=0
+            dfs(nx,ny)
+
+T=int(sys.stdin.readline().rstrip())
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+for i in range(T):
+
+    answer=0
+    M,N,K=map(int,sys.stdin.readline().rstrip().split(' '))
+    cabbage=[[0 for i in range(N)] for j in range(M)]
+
+    for j in range(K):
+        a,b=map(int,sys.stdin.readline().rstrip().split(' '))
+        cabbage[a][b]=1
+
+    for j in range(M):
+        for k in range(N):
+            if(cabbage[j][k]==1):
+                cabbage[j][k]=0
+                answer+=1
+                dfs(j,k)
+
+    print(answer)
