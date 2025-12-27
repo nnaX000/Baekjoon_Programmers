46fce43cc2e0f62ee900829613c64b377dc9df62
김나영 <kny200208@naver.com>
2025-12-27 23:05:13 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 600 ms, Memory: 66108 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -2,25 +2,32 @@ import sys
 
 sys.setrecursionlimit(10**6)
 
-N,M=map(int,sys.stdin.readline().rstrip().split(' '))
-graph=[[] for i in range(N+1)]
+input=sys.stdin.readline
+
+N,M=map(int,input().split(' '))
+
+edge=[[] for i in range(N+1)]
+visited=[False for i in range(N+1)]
+
 answer=0
 
+def dfs(node):
+    global visited
+
+    for i in range(len(edge[node])):
+        if(not visited[edge[node][i]]):
+            visited[edge[node][i]]=True
+            dfs(edge[node][i])
+
 for i in range(M):
-    a,b=map(int,sys.stdin.readline().rstrip().split(' '))
-    graph[a].append(b)
-    graph[b].append(a)
+    a,b=map(int,input().rstrip().split(' '))
 
-def dfs(start):
-    for i in range(len(graph[start])):
-        if(not visited[graph[start][i]]):
-            visited[graph[start][i]]=True
-            dfs(graph[start][i])
-
-visited=[False for i in range(N+1)]
+    edge[a].append(b)
+    edge[b].append(a)
 
 for i in range(1,N+1):
     if(not visited[i]):
+        visited[i]=True
         dfs(i)
         answer+=1
 
