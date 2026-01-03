ac6e40d0537c509a00a7c02664ba8d8b96a45bad
김나영 <kny200208@naver.com>
2026-01-03 16:41:18 +0900

    [Silver II] Title: 마인크래프트, Time: 136 ms, Memory: 37028 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,16 +1,19 @@
 import sys
+from collections import Counter
 
 input=sys.stdin.readline
 
 N,M,B=map(int,input().split())
 land=[list(map(int,input().split())) for _ in range(N)]
+land_c=[i for j in land for i in j]
+c=Counter(land_c)
 
 #블록을 제거하는게 더 큰 cost -> 차라리 쌓는게 나음.
 #땅 높이는 256블록 초과 X
 #심을수있는 블록수 정해져있음
 
-min_value=min([min(i) for i in land])
-max_value=max([max(i) for i in land])
+min_value=min(land_c)
+max_value=max(land_c)
 
 if(max_value>256):
     max_value=256
@@ -21,25 +24,21 @@ if(min_value<0):
 answer=float('inf')
 ht=0
 
-for i in range(0,266):
-    check=False
-
+for i in range(min_value,max_value+1):
     up=0
     down=0
 
-    for j in range(N):
-        for k in range(M):
-            if(i-land[j][k]>0): #기준이 높아서 쌓아야 함.
-                up+=i-land[j][k]
-            elif(i-land[j][k]<0): # 기준이 낮아서 지워야 함.
-                down+=abs(i-land[j][k])
+    for k,v in c.items():
+        if(i-k>0): #기준이 높아서 쌓아야 함.
+            up+=(i-k)*v
+        elif(i-k<0): # 기준이 낮아서 지워야 함.
+            down+=abs(i-k)*v
 
     if(up>down+B):
         continue
 
-    if(not check):
-        if(answer>=up+down*2):
-            answer=up+down*2  
-            ht=i
+    if(answer>=up+down*2):
+        answer=up+down*2  
+        ht=i
 
 print(answer,ht)
