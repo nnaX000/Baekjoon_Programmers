ecc55673322e5b0dfcc4909b5a3eb6baed11386f
김나영 <kny200208@naver.com>
2026-01-05 16:43:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1136 ms, Memory: 74100 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -7,8 +7,15 @@ N,M=map(int,input().split())
 array=[list(map(int,input().split())) for _ in range(N)]
 
 for i in range(N):
-    for j in range(1,N):
-        array[i][j]=array[i][j-1]+array[i][j]
+    for j in range(N):
+        if(i==0):
+            if(j!=0):
+                array[i][j]=array[i][j-1]+array[i][j]
+        else:
+            if(j==0):
+                array[i][j]=array[i][j]+array[i-1][j]
+            else:
+                array[i][j]=array[i][j]+array[i-1][j]+array[i][j-1]-array[i-1][j-1]
 
 for i in range(M):
     sum_value=0
@@ -19,10 +26,11 @@ for i in range(M):
     x_2-=1
     y_2-=1
 
-    for j in range(x_1,x_2+1):
-        if(y_1!=0):
-            sum_value+=(array[j][y_2]-array[j][y_1-1])
-        else:
-            sum_value+=array[j][y_2]
-
-    print(sum_value)
+    if(y_1-1<0 and x_1-1>=0):
+        print(array[x_2][y_2]-array[x_1-1][y_2])
+    elif(y_1-1>=0 and x_1-1<0):
+        print(array[x_2][y_2]-array[x_2][y_1-1])
+    elif(y_1-1>=0 and x_1-1>=0):
+        print(array[x_2][y_2]-array[x_2][y_1-1]-array[x_1-1][y_2]+array[x_1-1][y_1-1])
+    else:
+        print(array[x_2][y_2])
