c1660425df836bd5509940c6fa64c8fbde4e39ac
김나영 <kny200208@naver.com>
2026-01-05 18:12:15 +0900

    [Silver I] Title: 정수 삼각형, Time: 144 ms, Memory: 41524 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,16 +1,28 @@
-num=int(input())
+import sys
+
+input=sys.stdin.readline
+
+n=int(input())
+
+tri=[]
+
+for i in range(n):
+    tri.append(list(map(int,input().split())))
+
 dp=[]
-array=[]
-for i in range(num):
-    temp=list(map(int,input().split(' ')))
-    array.append(temp)
-dp=array.copy()
-for i in range(1,len(array)):
-    for j in range(len(array[i])):
+
+for i in range(1,n+1):
+    dp.append([float('-inf')]*i)
+
+dp[0][0]=tri[0][0]
+
+for i in range(1,n):
+    for j in range(len(dp[i])):
         if(j==0):
-            dp[i][j]=dp[i-1][0]+dp[i][j]
-        elif(j==len(array[i])-1):
-            dp[i][j] = dp[i-1][len(array[i])-2]+dp[i][j]
+            dp[i][j]=dp[i-1][0]+tri[i][j]
+        elif(j==len(dp[i])-1):
+            dp[i][j]=dp[i-1][-1]+tri[i][j]
         else:
-            dp[i][j]=max(dp[i-1][j-1]+dp[i][j],dp[i-1][j]+dp[i][j])
+            dp[i][j]=max(dp[i-1][j]+tri[i][j],dp[i-1][j-1]+tri[i][j])
+
 print(max(dp[-1]))
