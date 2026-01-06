01b815b8eb2d0064737675118a29153a08a72e37
김나영 <kny200208@naver.com>
2026-01-06 21:22:20 +0900

    [Silver III] Title: N과 M (5), Time: 152 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,24 +1,12 @@
 import sys
+from itertools import permutations
 
-N,M=map(int,sys.stdin.readline().rstrip().split(' '))
-array=list(map(int,sys.stdin.readline().rstrip().split(' ')))
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+
+array=list(map(int,input().split()))
 array.sort()
 
-def dfs(visited,array,tmp):
-    if(len(tmp)==M):
-        for j in tmp:
-            print(j,end=" ")
-        print()
-        return
-    
-    for i in range(len(array)):
-        if(not visited[i]):
-            tmp.append(array[i])
-            visited[i]=True
-            dfs(visited,array,tmp)
-            visited[i]=False
-            tmp.pop()
-
-visited=[False for i in range(len(array))]
-tmp=[]
-dfs(visited,array,tmp)
+for i in permutations(array,M):
+    print(*i)
