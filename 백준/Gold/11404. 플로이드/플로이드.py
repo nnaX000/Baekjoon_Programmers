4151382f1b5190d5d62994f9c4a02e5334604496
김나영 <kny200208@naver.com>
2026-01-08 16:58:01 +0900

    [Gold IV] Title: 플로이드, Time: 504 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,29 +1,29 @@
 import sys
 
-n = int(sys.stdin.readline().strip())
-m = int(sys.stdin.readline().strip())
+input=sys.stdin.readline
 
-distance=[[float('inf') for j in range(n+1)] for i in range(n+1)]
+n=int(input())
+m=int(input())
 
-for i in range(n+1):
-    distance[i][i]=0
+dp=[[float('inf') for _ in range(n+1)] for _ in range(n+1)]
 
 for i in range(m):
-    start,end,cost=map(int,sys.stdin.readline().strip().split(' '))
-    distance[start][end]=min(cost,distance[start][end])
+    a,b,c=map(int,input().split())
 
-for k in range(1,n+1):
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            distance[i][j]=min(distance[i][j],distance[i][k]+distance[k][j])
+    dp[a][b]=min(c,dp[a][b])
+
+for i in range(1,n+1):
+    dp[i][i]=0
 
 for i in range(1,n+1):
     for j in range(1,n+1):
-        if(distance[i][j]==float('inf')):
-            distance[i][j]=0
+        for k in range(1,n+1):
+            dp[j][k]=min(dp[j][k],dp[j][i]+dp[i][k])
 
-for i in range(1,n+1):
+for i in range(1,len(dp)):
     for j in range(1,n+1):
-        print(distance[i][j],end=" ")
-
+        if(dp[i][j]!=float('inf')):
+            print(dp[i][j],end=" ")
+        else:
+            print(0,end=" ")
     print()
