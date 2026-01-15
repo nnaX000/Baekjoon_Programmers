53744c9e2c72af6c885cb7d15c4d60f875b43287
김나영 <kny200208@naver.com>
2026-01-15 15:04:53 +0900

    [Gold IV] Title: 알파벳, Time: 1144 ms, Memory: 109968 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 input=sys.stdin.readline
 
@@ -9,22 +10,26 @@ dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 answer=float('-inf')
 
+visited_state=set()
+
 def dfs(x,y,visited,cost):
     global answer
+
+    if (x,y,visited) in visited_state:
+        return
+    visited_state.add((x,y,visited))
 
     for i in range(4):
         nx=x+dx[i]
         ny=y+dy[i]
 
-        if(0<=nx<R and 0<=ny<C and not visited[ord(board[nx][ny])-65]):
-            visited[ord(board[nx][ny])-65]=True
-            answer=max(answer,cost+1)
-            dfs(nx,ny,visited,cost+1)
-            visited[ord(board[nx][ny])-65]=False
+        if(0<=nx<R and 0<=ny<C):
+            bit = 1 << (ord(board[nx][ny])-65)
+            if(not bit & visited):
+                answer=max(answer,cost+1)
+                dfs(nx,ny,visited|bit,cost+1)
 
-
-visited=[False for _ in range(26)]
-visited[ord(board[0][0])-65]=True
+visited = 1 << (ord(board[0][0])-65)
 dfs(0,0,visited,1) #x,y,visited
 
 print(answer if answer!=float('-inf') else 1)
