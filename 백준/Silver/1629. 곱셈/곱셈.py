a9ef05d08d35d201937b904f66e0407145e97d85
NaYoung Kim <kny200208@naver.com>
2026-02-01 13:32:28 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -2,6 +2,17 @@ import sys
 
 input=sys.stdin.readline
 
-A,B,C=map(int,input().split())
+A,B,C=map(int,input().split()) # (A**B)%C
 
-print(pow(A,B,C))
+def calcul(n):
+    if(n==1):
+        return A
+    
+    tmp=calcul(n//2) % C
+
+    if(n%2==0):
+        return tmp*tmp
+    else:
+        return tmp*tmp*calcul(1)
+
+print(calcul(B)%C)
