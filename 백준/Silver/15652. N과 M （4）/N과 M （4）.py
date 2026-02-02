f5e57a5a9948e9876bba6c4fd4c3e6d83754bcf2
NaYoung Kim <kny200208@naver.com>
2026-02-02 13:41:44 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -3,16 +3,17 @@ import sys
 input=sys.stdin.readline
 
 N,M=map(int,input().split())
-array=[i for i in range(1,N+1)]
 
-def dfs(num,start):
-    if(len(num)==M):
-        print(*num)
+array=[i+1 for i in range(N)]
+
+def dfs(start,arr):
+    if(len(arr)==M):
+        print(*arr)
         return
     
     for i in range(start,len(array)):
-        num.append(array[i])
-        dfs(num,i)
-        num.pop()
+        arr.append(array[i])
+        dfs(i,arr)
+        arr.pop()
 
-dfs([],0)
+dfs(0,[])
