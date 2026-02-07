7a0dbec449e5c1243ec7abee1d5a8386bb02658e
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-07 11:59:42 +0900

    [Gold IV] Title: 연구소, Time: 1328 ms, Memory: 37684 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,25 +1,50 @@
 import sys
-from itertools import combinations
 from collections import deque
 
 input=sys.stdin.readline
 
 N,M=map(int,input().split())
+
 maps=[list(map(int,input().split())) for _ in range(N)]
 
-answer=float(('-inf'))
+#0이 빈칸, 1이 벽, 2가 바이러스
+#벽을 세개만 세워야 함.
 
 vacant=[]
 virus=[]
-wall=[]
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
+answer=float('-inf')
+
+for i in range(N):
+    for j in range(M):
+        if(maps[i][j]==0):
+            vacant.append([i,j])
+        elif(maps[i][j]==2):
+            virus.append([i,j])
+
+candi=[]
+# 벽 세울 경우의 수 구하기
+def dfs(start,arr):
+    global candi
+
+    if(len(arr)==3):
+        candi.append(arr[:])
+        return
+    
+    for i in range(start,len(vacant)):
+        arr.append(vacant[i])
+        dfs(i+1,arr)
+        arr.pop()
+
+dfs(0,[])
+
+#바이러스 퍼져나가는 거 체크
 def bfs():
-    global count
+    global maps_t
     global dequee
-    global visited
 
     while(dequee):
         x,y=dequee.popleft()
@@ -28,33 +53,29 @@ def bfs():
             nx=x+dx[i]
             ny=y+dy[i]
 
-            if(0<=nx<N and 0<=ny<M and maps[nx][ny]==0 and (nx,ny) not in visited):
-                visited.add((nx,ny))
-                count+=1
+            if(0<=nx<N and 0<=ny<M and maps_t[nx][ny]==0):
+                maps_t[nx][ny]=2
                 dequee.append((nx,ny))
 
-for i in range(N):
-    for j in range(M):
-        if(maps[i][j]==0):
-            vacant.append([i,j])
-        elif(maps[i][j]==2):
-            virus.append([i,j])
-        else:
-            wall.append([i,j])
 
-for i in combinations(vacant,3):
-    for a,b in i:
-        maps[a][b]=1
+for i in range(len(candi)):
+    maps_t=[j[:] for j in maps]
 
-    count=0
+    # 벽 세우기
+    for j in range(len(candi[i])):
+        x,y=candi[i][j][0],candi[i][j][1]
+        maps_t[x][y]=1
+    
     dequee=deque(virus)
-    visited=set()
 
     bfs()
 
-    answer=max(answer,N*M-len(virus)-len(wall)-count-3)
+    tmp=0
+    for j in range(N):
+        for k in range(M):
+            if(maps_t[j][k]==0):
+                tmp+=1
 
-    for a,b in i:
-        maps[a][b]=0
+    answer=max(answer,tmp)
 
 print(answer)
