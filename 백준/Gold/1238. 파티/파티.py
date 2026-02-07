2c50ba3841a89bb5114219bb65717f9798545437
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-07 13:21:19 +0900

    [Gold III] Title: 파티, Time: 52 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,46 +1,58 @@
 import sys
 import heapq
 
-def dij():
-    global result
-    global heap
-
-    while(heap):
-        cost,current=heapq.heappop(heap)
-
-        if(result[current]<cost):
-            continue
-
-        for i in range(len(road[current])):
-            if(result[road[current][i][0]]>result[current]+road[current][i][1]):
-                result[road[current][i][0]]=result[current]+road[current][i][1]
-                heapq.heappush(heap,(result[road[current][i][0]],road[current][i][0]))
-
 input=sys.stdin.readline
 
-N,M,X=map(int,input().split()) # X가 목적지
+N,M,X=map(int,input().split())
 
-road=[[] for _ in range(N+1)]
+town=[[] for _ in range(N+1)]
+town_r=[[] for _ in range(N+1)]
+
 answer=[0 for _ in range(N+1)]
 
+def dij():
+    global heap
+    global result
+
+    while(heap):
+        cost,x=heapq.heappop(heap)
+
+        if(result[x]<cost):
+            continue
+
+        if(cost==0 and x==X):
+            result[x]=0
+
+        for i in range(len(town[x])):
+            if(result[town[x][i][0]]>cost+town[x][i][1]):
+                result[town[x][i][0]]=cost+town[x][i][1]
+                heapq.heappush(heap,(cost+town[x][i][1],town[x][i][0]))
+
 for i in range(M):
-    a,b,cost=map(int,input().split())
-    road[a].append([b,cost])
+    start,end,T=map(int,input().split())
 
-for i in range(1,N+1):
-    heap=[]
-    heapq.heapify(heap)
+    town[start].append([end,T])
+    town_r[end].append([start,T])
 
-    heapq.heappush(heap,(0,i)) #cost, 위치
-    result=[float('inf') for _ in range(N+1)]
-    result[i]=0
+heap=[]
+heapq.heapify(heap)
+heapq.heappush(heap,(0,X)) # cost, X
+result=[float('inf') for _ in range(N+1)]
+dij()
 
-    dij()
+for i in range(N+1):
+    if(result[i]!=float('inf')):
+        answer[i]+=result[i]
 
-    if(i==X):
-        for i in range(1, N+1):
-            answer[i] += result[i]
-    else:
-        answer[i]+=result[X]
+heap=[]
+heapq.heapify(heap)
+heapq.heappush(heap,(0,X))
+town=[i[:] for i in town_r]
+result=[float('inf') for _ in range(N+1)]
+dij()
+
+for i in range(N+1):
+    if(result[i]!=float('inf')):
+        answer[i]+=result[i]
 
 print(max(answer))
