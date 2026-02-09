a9c755cc316bccfadd7cbc19ce9ef46a6d6a364e
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-09 20:09:16 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 6208 ms, Memory: 206436 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,40 +1,36 @@
 import sys
 from collections import deque
 
-N,M=map(int,sys.stdin.readline().rstrip().split(' '))
-graph=[list(map(int,sys.stdin.readline().rstrip())) for i in range(N)]
-min_value=float('inf')
+input=sys.stdin.readline
+
+# 벽 한개까지 부수고 이동 가능
+# 1은 이동할 수 없는 벽
+
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
+N,M=map(int,input().split())
+maps=[list(map(int,input().rstrip())) for _ in range(N)]
 dequee=deque()
-dequee.append([0,0,0])
-visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
+dequee.append((0,0,0))
+visited=[[[float('inf'),float('inf')] for _ in range(M)] for _ in range(N)]
+
 visited[0][0][0]=1
+visited[0][0][1]=1
 
 while(dequee):
-    tmp=dequee.popleft()
-    nx=tmp[0]
-    ny=tmp[1]
-    count=tmp[2]
+    x,y,b=dequee.popleft()
 
-    if(nx==N-1 and ny==M-1):
-        min_value=min(min_value,visited[nx][ny][count])
-        break
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
 
-    for j in range(4):
-        nx_x=nx+dx[j]
-        ny_y=ny+dy[j]
+        if(0<=nx<N and 0<=ny<M):
+            if(maps[nx][ny]==0 and visited[nx][ny][b]==float('inf')):
+                dequee.append((nx,ny,b))
+                visited[nx][ny][b]=visited[x][y][b]+1
+            elif(maps[nx][ny]==1 and b==0 and visited[nx][ny][1]==float('inf')):
+                dequee.append((nx,ny,1))
+                visited[nx][ny][1]=visited[x][y][b]+1
 
-        if(0<=nx_x<N and 0<=ny_y<M and visited[nx_x][ny_y][count]==0):
-            if(graph[nx_x][ny_y]==0):
-                dequee.append([nx_x,ny_y,count])
-                visited[nx_x][ny_y][count]=visited[nx][ny][count]+1
-            elif(graph[nx_x][ny_y]==1 and count==0):
-                dequee.append([nx_x,ny_y,count+1])
-                visited[nx_x][ny_y][count+1]=visited[nx][ny][count]+1
-
-if(min_value==float('inf')):
-    print(-1)
-else:
-    print(min_value)
+print(min(visited[N-1][M-1]) if min(visited[N-1][M-1])!=float('inf') else -1)
