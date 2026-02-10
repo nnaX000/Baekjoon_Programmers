6345457941d166d5db9f3cbe01039f6055d5e6e5
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-10 16:29:02 +0900

    [level 2] Title: 가장 큰 수, Time: 441.43 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -3,15 +3,7 @@ from functools import cmp_to_key
 def solution(numbers):
     s_n=list(map(str,numbers))
     
-    def compare(a,b):
-        if(int(a+b)>int(b+a)):
-            return -1
-        elif(int(a+b)<int(b+a)):
-            return 1
-        return 0
-    
-    s_n.sort(key=cmp_to_key(compare))
-    
+    s_n.sort(key = lambda x: x*3, reverse = True)
     answer=''.join(s_n)
     
-    return answer if int(answer)!=0 else "0"
+    return answer if int(answer)!=0 else '0'
