f6be3b79b7df50b9ddd81f3c729150b856eb0685
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-16 16:01:50 +0900

    [Silver II] Title: 최소 힙, Time: 104 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -4,17 +4,18 @@ import heapq
 input=sys.stdin.readline
 
 heap=[]
+
 heapq.heapify(heap)
 
 N=int(input())
 
 for i in range(N):
-    tmp=int(input())
+    x=int(input())
 
-    if(tmp==0):
+    if(x==0):
         if(heap):
             print(heapq.heappop(heap))
         else:
             print(0)
     else:
-        heapq.heappush(heap,tmp)
+        heapq.heappush(heap,x)
