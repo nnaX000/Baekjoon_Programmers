9399479287b2cc5b1a1f2ecbaa155d1d32f2236d
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-17 23:49:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -5,8 +5,7 @@ input=sys.stdin.readline
 N=int(input())
 
 maps=[list(map(int,input().rstrip())) for _ in range(N)]
-
-cost=0
+visited=[[False for _ in range(N)] for _ in range(N)]
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
@@ -14,26 +13,27 @@ dy=[0,0,-1,1]
 answer=[]
 
 def dfs(x,y):
-    global cost
+    global visited
+    global count
 
     for i in range(4):
         nx=x+dx[i]
         ny=y+dy[i]
 
-        if(0<=nx<N and 0<=ny<N and maps[nx][ny]==1):
-            maps[nx][ny]=0
+        if(0<=nx<N and 0<=ny<N and maps[nx][ny]==1 and not visited[nx][ny]):
+            visited[nx][ny]=True
+            count+=1
             dfs(nx,ny)
-            cost+=1
 
 for i in range(N):
     for j in range(N):
-        if(maps[i][j]==1):
-            maps[i][j]=0
-            cost=1
+        if(maps[i][j]==1 and not visited[i][j]):
+            visited[i][j]=True
+            count=1
             dfs(i,j)
-            answer.append(cost)
+            answer.append(count)
 
-answer.sort()
 print(len(answer))
+answer.sort()
 for i in answer:
     print(i)
