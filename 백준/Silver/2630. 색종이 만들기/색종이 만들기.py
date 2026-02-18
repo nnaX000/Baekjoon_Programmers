33ad6b9aedd6cc8032a43af570490f07e19ae913
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-18 21:27:02 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -3,48 +3,42 @@ import sys
 input=sys.stdin.readline
 
 N=int(input())
+paper=[list(map(int,input().split())) for _ in range(N)]
 
-paper=[list(map(int,input().rstrip().split())) for _ in range(N)]
-length=len(paper)
+white=0
+blue=0
 
-sum_value=[0,0]
+def dfs(x,y,length):
+    global white
+    global blue
 
-visited=set()
+    if(length==1):
+        if(paper[x][y]==0):
+            white+=1
+        else:
+            blue+=1
+        return
 
-stand=length
+    check=False
+    stand=paper[x][y]
+    
+    for i in range(length):
+        for j in range(length):
+            if(paper[x+i][y+j]!=stand):
+                check=True
+                break
 
-while(stand>=1):
-    for i in range(0,length,stand): # x시작점 잡아줌
-        for j in range(0,length,stand): # y시작점 잡아줌
-            check=False
-            s=0
-            tmp_visited=set()
-            for k in range(i,i+stand):
-                for l in range(j,j+stand):
-                    if((k,l) not in visited):
-                        if(k==i and l==j):
-                            s=paper[k][l]
-                            tmp_visited.add((k,l))
-                        elif(paper[k][l]!=s):
-                            check=True
-                            break
-                        elif(paper[k][l]==s):
-                            tmp_visited.add((k,l))
-                    else:
-                        check=True
-                        break
-                
-                if(check):
-                    break
+    if(check):
+        dfs(x,y,length//2)
+        dfs(x+length//2,y,length//2)
+        dfs(x,y+length//2,length//2)
+        dfs(x+length//2,y+length//2,length//2)
+    else:
+        if(stand==0):
+            white+=1
+        else:
+            blue+=1
 
-            if(not check):
-                if(s==0):
-                    sum_value[0]+=1
-                else:
-                    sum_value[1]+=1
-
-                visited=visited.union(tmp_visited)
-            
-    stand//=2
-
-print(sum_value[0],sum_value[1])
+dfs(0,0,N)
+print(white)
+print(blue)
