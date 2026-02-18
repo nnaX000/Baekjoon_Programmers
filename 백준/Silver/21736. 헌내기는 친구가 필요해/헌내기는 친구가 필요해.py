65e682c0cbf3d3397936b5ca58318d92b5002a05
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-18 22:28:07 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 652 ms, Memory: 112500 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,39 +1,42 @@
 import sys
-from collections import deque
+
+sys.setrecursionlimit(10**6)
 
 input=sys.stdin.readline
 
 N,M=map(int,input().split())
 
-campus=[list(input().rstrip()) for _ in range(N)]
+campus=[list(input()) for _ in range(N)]
 
-do_x=0
-do_y=0
+# O는 빈 공간, X는 벽, I는 도연이, P는 사람
+visited=[[False for _ in range(M)] for _ in range(N)]
+d_x=0
+d_y=0
+answer=0
 
 for i in range(N):
     for j in range(M):
         if(campus[i][j]=="I"):
-            do_x,do_y=i,j
+            d_x=i
+            d_y=j
+            campus[i][j]="O"
+            break
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-q=deque()
-q.append((do_x,do_y))
-
-answer=0
-
-while(q):
-    x,y=q.popleft()
+def dfs(x,y):
+    global answer
 
     for i in range(4):
         nx=x+dx[i]
         ny=y+dy[i]
 
-        if(0<=nx<N and 0<=ny<M and campus[nx][ny]!="X"):
+        if(0<=nx<N and 0<=ny<M and campus[nx][ny]!="X" and not visited[nx][ny]):
+            visited[nx][ny]=True
             if(campus[nx][ny]=="P"):
                 answer+=1
-            campus[nx][ny]="X"
-            q.append((nx,ny))
+            dfs(nx,ny)
 
+dfs(d_x,d_y)
 print(answer if answer!=0 else "TT")
