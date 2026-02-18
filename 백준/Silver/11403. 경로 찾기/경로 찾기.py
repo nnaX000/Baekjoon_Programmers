53df6be7096021e698a67dc561cd9c79fd159cef
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-18 18:59:31 +0900

    [Silver I] Title: 경로 찾기, Time: 92 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -3,14 +3,19 @@ import sys
 input=sys.stdin.readline
 
 N=int(input())
+graph=[list(map(int,input().split())) for _ in range(N)]
+dp=[[0 for _ in range(N)] for _ in range(N)]
 
-arr=[list(map(int,input().rstrip().split())) for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if(graph[i][j]==1):
+            dp[i][j]=1
 
-for k in range(N):
-    for i in range(N):
-        for j in range(N):
-            if(arr[i][k]==1 and arr[k][j]==1):
-                arr[i][j]=1
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if(dp[j][k]==0 and dp[j][i]==1 and dp[i][k]==1):
+                dp[j][k]=1
 
-for i in arr:
-    print(*i)
+for i in range(N):
+    print(*dp[i])
