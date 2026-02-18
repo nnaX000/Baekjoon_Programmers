8ad95d52500ebf13169d74bec12b578af9ef74bb
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-18 19:23:00 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -3,21 +3,16 @@ import sys
 input=sys.stdin.readline
 
 T=int(input())
+dp=[[0,0] for _ in range(41)]
 
-count=[[0,0] for i in range(41)]
+dp[0]=[1,0]
+dp[1]=[0,1]
+dp[2]=[1,1]
 
-count[0][0]=1
-count[1][1]=1
-
-arr=[]
+for i in range(3,41):
+    dp[i][0]=dp[i-1][0]+dp[i-2][0]
+    dp[i][1]=dp[i-1][1]+dp[i-2][1]
 
 for i in range(T):
-    N=int(input())
-    arr.append(N)
-
-for i in range(2,max(arr)+1):
-    count[i][0]=count[i-1][0]+count[i-2][0]
-    count[i][1]=count[i-1][1]+count[i-2][1]
-
-for i in arr:
-    print(count[i][0],count[i][1])
+    tmp=int(input())
+    print(dp[tmp][0],dp[tmp][1])
