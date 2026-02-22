fdd4299c656127dd59f20837b6b77f06d39f93f1
NaYoung Kim <nnnyyy0819@naver.com>
2026-02-22 18:52:22 +0900

    [level 3] Title: 아이템 줍기, Time: 2.27 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,42 +1,44 @@
 from collections import deque
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
-    boards=[[0 for i in range(101)] for j in range(101)]
-    dequee=deque()
-    visited=[[False for i in range(101)] for j in range(101)]
-    dequee.append((characterX*2,characterY*2,0,visited)) # 위치, cost, visited
+    board=[[-1 for _ in range(101)] for _ in range(101)]
     
     dx=[-1,1,0,0]
     dy=[0,0,-1,1]
     
-    # 일단 직사각형 다 칠하기
-    for a,b,c,d in rectangle:
-        for i in range(a*2,(c*2)+1):
-            for j in range(b*2,(d*2)+1):
-                boards[i][j]=1 # 접근 가능
-    
-    # 테두리 빼고 다 접근불가 처리
-    for a,b,c,d in rectangle:
-        for i in range((a*2)+1,c*2):
-            for j in range((b*2)+1,d*2):
-                boards[i][j]=0
+    def dfs(x,y):
+        nonlocal visited
+        
+        dq=deque()
+        dq.append((x,y,0))
+        
+        while(dq):
+            cx,cy,count=dq.popleft()
+            
+            if(cx==itemX*2 and cy==itemY*2):
+                return count
+            
+            for i in range(4):
+                nx=cx+dx[i]
+                ny=cy+dy[i]
                 
-    while(dequee):
-        x,y,cost,visited=dequee.popleft()
-        
-        if(x==itemX*2 and y==itemY*2):
-            answer=cost
-            break
-        
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
+                if(0<=nx<101 and 0<=ny<101 and board[nx][ny]==2 and not visited[nx][ny]):
+                    visited[nx][ny]=True
+                    dq.append((nx,ny,count+1))
             
-            n_visited=[j[:] for j in visited]
-            
-            if(0<=nx<101 and 0<=ny<101 and boards[nx][ny]==1 and not n_visited[nx][ny]):
-                n_visited[nx][ny]=True
-                dequee.append((nx,ny,cost+1,n_visited))
+    #테두리 칠하기
+    for i in range(len(rectangle)):
+        s_x,s_y,e_x,e_y=rectangle[i][0]*2,rectangle[i][1]*2,rectangle[i][2]*2,rectangle[i][3]*2
+        for j in range(s_x,e_x+1):
+            for k in range(s_y,e_y+1):
+                if(j==s_x or j==e_x or k==s_y or k==e_y):
+                    if(board[j][k]==-1):
+                        board[j][k]=2 #선이 2
+                else:
+                    board[j][k]=1
     
-    return answer//2
+    count=0
+    visited=[[False for _ in range(101)] for _ in range(101)]
+    visited[characterX*2][characterY*2]=True
+    
+    return dfs(characterX*2,characterY*2)//2
