5a38ea38893934c182d83a11597c8aaf1a3a2b23
NaYoung Kim <nnnyyy0819@naver.com>
2026-03-05 22:45:51 +0900

    [Gold III] Title: 우주신과의 교감, Time: 1400 ms, Memory: 133280 KB -BaekjoonHub

---
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
@@ -0,0 +1,53 @@
+import sys
+
+input=sys.stdin.readline
+
+answer=0
+
+def find(x):
+    if(parent[x]==x):
+        return x
+    return find(parent[x])
+
+def union(x,y):
+    px,py=find(x),find(y)
+
+    if(px==py):
+        return False
+    elif(rank[px]<rank[py]):
+        parent[px]=py
+    elif(rank[py]<rank[px]):
+        parent[py]=px
+    elif(rank[px]==rank[py]):
+        parent[py]=px
+        rank[px]+=1
+    return True
+
+N,M=map(int,input().split())
+parent=[i for i in range(N+1)]
+rank=[0 for _ in range(N+1)]
+
+# 통로들의 길이는 2차원 좌표계상의 거리
+distance=[]
+
+tmp=[list(map(int,input().split())) for _ in range(N)]
+
+for i in range(N-1):
+    for j in range(i+1,N):
+        dx = tmp[i][0] - tmp[j][0]
+        dy = tmp[i][1] - tmp[j][1]
+        result = (dx*dx + dy*dy) ** 0.5
+
+        distance.append([result,i+1,j+1])
+
+distance.sort(key=lambda x:x[0])
+
+for i in range(M):
+    x,y=map(int,input().split())
+    union(x,y)
+
+for cost,start,end in distance:
+    if (union(start,end)):
+        answer+=cost
+
+print(f"{answer:.2f}")
