bf7bf7d2302a0a235e00a790834eea2911843ff4
NaYoung Kim <nnnyyy0819@naver.com>
2026-03-07 12:54:52 +0900

    [level 3] Title: 양과 늑대, Time: 10.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,22 +1,13 @@
-import sys
-
 def solution(info, edges):
     # 당신이 모은 양의 수보다 늑대의 수가 같거나 더 많아지면 바로 모든 양을 잡아먹어 버립니다. 
     # 0이 양, 1이 늑대
     
     answer=float('-inf')
     
-    sys.setrecursionlimit(10**6)
-    
     ways=[[] for _ in range(len(info)+1)]
-    parents=[0 for _ in range(len(info)+1)]
-    visited=[0 for _ in range(len(info)+1)]
     
     for a,b in edges:
         ways[a].append(b)
-        parents[b]=a
-        
-    parents[0]=-1
         
     def dfs(avail,s_n,w_n):
         nonlocal answer
@@ -27,7 +18,7 @@ def solution(info, edges):
         answer=max(answer,s_n)
             
         for idx,i in enumerate(avail):
-            tmp=avail[:idx]+avail[idx+1:]+ways[i]
+            tmp=avail[:idx]+avail[idx+1:]+ways[i] # 현재 방문한 노드 빼버림
             
             if(info[i]==1):
                 dfs(tmp,s_n,w_n+1)
