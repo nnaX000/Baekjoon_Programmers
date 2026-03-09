b764efe3755597f2594bd4e0e512760c97e7bfef
NaYoung Kim <nnnyyy0819@naver.com>
2026-03-09 13:22:29 +0900

    [Gold III] Title: 치즈, Time: 700 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,70 +1,83 @@
 import sys
+from collections import deque
 
 input=sys.stdin.readline
 
-sys.setrecursionlimit(10**6)
-
 N,M=map(int,input().split())
-cheeze=[list(map(int,input().split())) for _ in range(N)]
-visited=[[False for _ in range(M)] for _ in range(N)]
+cheeze=[list(map(int,input().split())) for _ in range(N)] #치즈 1, 치즈0
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-answer=0
+c_n=0
+out_x=0
+out_y=0
+cumulate=0
+time=0
 
-# 실내온도 공기가 있는 칸 체크하기
-def air(x,y):
+for i in range(N):
+    for j in range(M):
+        if(cheeze[i][j]==1):
+            c_n+=1
+        else:
+            if(out_x==0 and out_y==0):
+                out_x,out_y=i,j
+
+if(c_n==0):
+    print(0)
+    sys.exit(0)
+
+def air():
     global cheeze
-    global visited
 
-    for i in range(4):
-        nx=x+dx[i]
-        ny=y+dy[i]
+    dq=deque()
+    dq.append((out_x,out_y))
+    cheeze[out_x][out_y]=-1
 
-        if(0<=nx<N and 0<=ny<M and cheeze[nx][ny]==0 and not visited[nx][ny]):
-            visited[nx][ny]=True
-            cheeze[nx][ny]=2 # 실내 공기->2
-            air(nx,ny)
-
-
-visited[0][0] = True
-cheeze[0][0] = 2
-air(0,0)
-tmp=[i[:] for i in cheeze]
-
-while(True):
-    melt=[]
     visited=[[False for _ in range(M)] for _ in range(N)]
-    visited[0][0]=True
-    cheeze[0][0]=0
+
+    while(dq):
+        x,y=dq.popleft()
+
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+
+            if(0<=nx<N and 0<=ny<M and cheeze[nx][ny]!=1 and not visited[nx][ny]):
+                visited[nx][ny]=True
+                cheeze[nx][ny]=-1
+                dq.append((nx,ny))
+
+def vanish():
+    global cheeze
+    global cumulate
+
+    change=[]
 
     for i in range(N):
         for j in range(M):
-            if(cheeze[i][j]==2):
-                cheeze[i][j]=0
 
-    air(0,0)
-
-    for i in range(N):
-        for j in range(M):
             if(cheeze[i][j]==1):
-                cnt=0
+                count=0
                 for k in range(4):
                     nx=i+dx[k]
                     ny=j+dy[k]
 
-                    if(0<=nx<N and 0<=ny<M and cheeze[nx][ny]==2):
-                        cnt+=1
+                    if(0<=nx<N and 0<=ny<M and cheeze[nx][ny]==-1):
+                        count+=1
+                
+                if(count>1):
+                    change.append([i,j])
+                    cumulate+=1
 
-                if(cnt>=2):
-                    melt.append([i,j])
+    for a,b in change:
+        cheeze[a][b]=0
 
-    if(not melt):
-        print(answer)
+while(True):
+    air()
+    vanish()
+    time+=1
+
+    if(cumulate==c_n):
+        print(time)
         break
-    else:
-        answer+=1
-
-    for x,y in melt:
-        cheeze[x][y]=0
