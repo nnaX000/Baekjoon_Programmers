b2429303419be8762c5c1f3b83693d8b9ae8302a
NaYoung Kim <nnnyyy0819@naver.com>
2026-03-09 15:26:27 +0900

    [Gold II] Title: 피보나치 수 6, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -1,18 +1,16 @@
 import sys
 
-sys.setrecursionlimit(10**6)
-
 input=sys.stdin.readline
 
-N=int(input())
+n=int(input())
 
 def calcul(k,m):
     result=[[0,0],[0,0]]
 
-    result[0][0]=((k[0][0]*m[0][0])+(k[0][1]*m[1][0])) % 1000000007
-    result[0][1]=((k[0][0]*m[0][1])+(k[0][1]*m[1][1])) % 1000000007
-    result[1][0]=((k[1][0]*m[0][0])+(k[1][1]*m[1][0])) % 1000000007
-    result[1][1]=((k[1][0]*m[0][1])+(k[1][1]*m[1][1])) % 1000000007
+    result[0][0]=(k[0][0]*m[0][0]+k[0][1]*m[1][0])%1000000007
+    result[0][1]=(k[0][0]*m[0][1]+k[0][1]*m[1][1])%1000000007
+    result[1][0]=(k[0][0]*m[1][0]+k[1][0]*m[1][1])%1000000007
+    result[1][1]=(k[0][1]*m[1][0]+k[1][1]*m[1][1])%1000000007
 
     return result
 
@@ -20,15 +18,15 @@ def fibo(n):
     if(n==1):
         return [[1,1],[1,0]]
     
-    tmp=fibo(n//2)
-    tmp_r=calcul(tmp,tmp)
+    half=fibo(n//2)
+    h_r=calcul(half,half)
 
     if(n%2==0):
-        return tmp_r
+        return h_r
     else:
-        return calcul(tmp_r,[[1,1],[1,0]])
-
-result=fibo(N)
+        return calcul(h_r,[[1,1],[1,0]])
+    
+result=fibo(n)
 
 for i in range(2):
     for j in range(2):
